9156a629e68335d29910119638419cf1b052de21
JPcookie290 <119297475+JPcookie290@users.noreply.github.com>
2024-09-10 18:18:41 +0200

    Update BoardTamriel.java
    Added some List to TamrilBoard

--- src/BoardTamriel.java
+++ src/BoardTamriel.java
@@ -11,20 +11,26 @@ public class BoardTamriel extends Board{
     // creates and adds territories to the continents
     /*public void createTerritories() {
         // Continent Aldmeri Dominion
-        String[] continentOneList = new String[]{"Khenathi's Roost", "Grathwood", "Greenshade", "Malabal Tor", "Reaper's March", "West Weals", "Auridon", "Summerset", "Elsweyr"};
-        for ( String territoryName : continentOneList ){
+        String[] continentOneList = new String[]{"Khenathi's Roost", "Grathwood", "Greenshade", "Malabal Tor", "Reaper's March", "West Weals", "Auridon", "Summerset", "Northen Elsweyr", "Southern Elsweyr"};
+  		String[] continentOneAbbr = new String[]{"KR", "GRA", "GRE", "MT", "RM", "WW", "A", "SI", "NE", "SE"};
+		Color[] continentOneColors = new Color[]{};      
+		for ( String territoryName : continentOneList ){
             Territory newTerritory = new Territory(territoryName, "Aldmeri Dominion");
             getContinent("Aldmeri Dominion").addTerritory(newTerritory);
         }
         // Continent Ebonheart Pact
-        String[] continentTwoList = new String[]{"Shadowfen", "Deshaan", "Stonefalls", "The Rift", "Esstmarch", "Vardenfell", "Telvanni Peninsula"};
-        for ( String territoryName : continentTwoList ){
+        String[] continentTwoList = new String[]{"Shadowfen", "Deshaan", "Stonefalls", "The Rift", "Eastmarch", "Vardenfell", "Telvanni Peninsula", "Bal Foyen", "Bleakrock Isle"};
+  		String[] continentTwoAbbr = new String[]{"SH", "D", "SF", "TR", "E", "V", "TP", "BF", "BI"};      
+		Color[] continentTwoColors = new Color[]{};
+		for ( String territoryName : continentTwoList ){
             Territory newTerritory = new Territory(territoryName, "Ebonheart Pact");
             getContinent("Ebonheart Pact").addTerritory(newTerritory);
         }
         // Continent Daggerfall Covenent
-        String[] continentThreeList = new String[]{"Glenumbra", "Rivenspire", "Stormhaven", "Wrothgar", "Alk'r Desert", "Bankorai", "High Isle and Galen", "Craglorn"};
-        for ( String territoryName : continentThreeList ){
+        String[] continentThreeList = new String[]{"Glenumbra", "Rivenspire", "Stormhaven", "Wrothgar", "Alk'r Desert", "Bankorai", "High Isle", "Galen", "Craglorn", "Betnikh", "The Reach", "Western Skyrim"};
+  		String[] continentThreeAbbr = new String[]{"GL", "R", "SH", "W", "AD", "HI", "GA", "C", "B", "RA", "WS"};      
+		Color[] continentThreeColors = new Color[]{};
+		for ( String territoryName : continentThreeList ){
             Territory newTerritory = new Territory(territoryName, "Daggerfall Covenent");
             getContinent("Daggerfall Covenent").addTerritory(newTerritory);
         }
